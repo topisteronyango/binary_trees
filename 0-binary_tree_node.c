389ece37d694a132772a9428568b5ce2635a2e4c
topisteronyango <t.onyango@alustudent.com>
2021-12-01 05:34:05 -0800

    ninary tress

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node that creates a binary tree node
-* @parent: pointer to the parent node of the node to create
-* @value: value to put in the new node
-* Return: pointer to the new node, or NULL on failure
+* binary_tree_node - creates a binary tree node.
+* @parent: pointer to the parent node of the node to create.
+* @value: value to store in the new node.
+* Return: pointer to the new node, or NULL on failure.
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -25,9 +25,9 @@ binary_tree_t *remover_izq(binary_tree_t *a_remover)
 }
 
 /**
- * binary_tree_insert_left  inserts a node to the left-child to a node
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
+ * binary_tree_insert_left - inserts a node as the left-child of a node.
+ * @parent: pointer to the node to insert the left-child in.
+ * @value: value to store in the new node.
  * Return: pointer to the new node, or NULL on failure or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
