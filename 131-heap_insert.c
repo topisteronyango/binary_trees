5a05f1e64331b1685e55836bdc5702927d0a1a8b
topisteronyango <t.onyango@alustudent.com>
2021-12-01 06:10:44 -0800

    binary trees

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -12,18 +12,4 @@ heap_t *heap_insert(heap_t **root, int value)
 	if (!root || !*root)
 		return (NULL);
 	return (*root);
-}#include "binary_trees.h"
-
-/**
-* heap_insert - pending.
-* @root: pending.
-* @value: pending.
-* Return: pending.
-*/
-heap_t *heap_insert(heap_t **root, int value)
-{
-	(void) value;
-	if (!root || !*root)
-		return (NULL);
-	return (*root);
 }
